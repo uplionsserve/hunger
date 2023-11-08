cdbec0b071548d3421dd4867900db050b0388463
Harrison Bouche <harrison@sm.media>
2023-11-08 15:35:59 -0500

    wip: updating added prettier, updating hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
-import sitemap from "@astrojs/sitemap";
-import partytown from "@astrojs/partytown";
-import node from "@astrojs/node";
-
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import svelte from "@astrojs/svelte"
+import sitemap from "@astrojs/sitemap"
+import partytown from "@astrojs/partytown"
+import node from "@astrojs/node"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), sitemap(), partytown()],
-  output: "server",
-  adapter: node({
-    mode: "middleware"
-  })
-});
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		svelte(),
+		sitemap(),
+		partytown(),
+	],
+	base: "/hunger",
+	output: "server",
+	adapter: node({
+		mode: "middleware",
+	}),
+})
