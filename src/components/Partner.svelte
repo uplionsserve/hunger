754243c5a7132d868b7a168b7d7a6f63d7ef8830
Harrison Bouche <harrison@sm.media>
2023-11-21 09:40:51 -0500

    fix: breaks if image sizes doesn't exist

--- src/components/Partner.svelte
+++ src/components/Partner.svelte
@@ -18,7 +18,9 @@
 		<div class="bg-primary absolute h-[4.5rem] w-[4.5rem] -translate-x-2"></div>
 		<div class="absolute h-[4.5rem] w-[4.5rem] -translate-y-2 bg-black"></div>
 		<img
-			src={`${import.meta.env.CMS_URL}${partner.logo_image.sizes.logo.url}`}
+			src={`${import.meta.env.CMS_URL}${
+				partner.logo_image.sizes?.logo.url ?? ""
+			}`}
 			alt=""
 			class="relative block h-16 w-16 shrink-0 list-none bg-white p-2"
 		/>
