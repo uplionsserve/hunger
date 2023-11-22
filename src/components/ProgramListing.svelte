78928f44857eb6178ce812d2068ca2b2dfec81bf
Harrison Bouche <harrison@sm.media>
2023-11-22 09:02:03 -0500

    fix: logo and props

--- src/components/ProgramListing.svelte
+++ src/components/ProgramListing.svelte
@@ -2,24 +2,54 @@
 	type Program = {
 		title: string
 		title_id: string
+		logo_image: {
+			sizes: {
+				logo: {
+					url: string
+				}
+			}
+			alt: string
+		}
 		description_html: string
+		cta_button: {
+			text: string
+			url: string
+		}
 	}
 
 	export let program: Program
 </script>
 
-<div
+<section
 	id={program.title_id}
-	class="root md:max-w-1xl flex flex-col gap-4 md:mx-auto lg:col-span-8"
+	class="root md:max-w-1xl flex min-h-[300px] flex-col gap-4 md:mx-auto lg:col-span-8"
 >
-	<h2
-		class="mb-4 mt-1 text-4xl font-extrabold uppercase leading-10 tracking-tight text-gray-900 sm:text-6xl sm:leading-none lg:text-5xl xl:text-6xl"
-	>
-		{program.title}
-	</h2>
+	<header class="mb-4 mt-1 flex items-center gap-2">
+		{#if program.logo_image}
+			<img
+				src={`${program.logo_image.sizes?.logo.url ?? ""}`}
+				alt=""
+				class="h-20 w-20"
+			/>
+		{/if}
+		<h2
+			class="text-4xl font-extrabold uppercase leading-10 tracking-tight text-gray-900 sm:text-6xl sm:leading-none lg:text-5xl xl:text-6xl"
+		>
+			{program.title}
+		</h2>
+	</header>
 
 	{@html program.description_html}
-</div>
+
+	{#if program.cta_button.url}
+		<a
+			href={program.cta_button.url}
+			target="_blank"
+			class="bg-primary w-fit px-5 py-3 font-bold uppercase text-white no-underline hover:brightness-90"
+			>{program.cta_button.text}</a
+		>
+	{/if}
+</section>
 
 <style>
 	.root :global(p) {
@@ -41,7 +71,7 @@
 	}
 
 	.root :global(ul) {
-		@apply list-inside list-disc font-body text-base text-gray-600;
+		@apply font-body list-inside list-disc text-base text-gray-600;
 
 		@media (min-width: 640px) {
 			font-size: 1.25rem /* 20px */;
