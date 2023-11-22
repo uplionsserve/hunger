18196c3dfe8be6296047109605fec12d0ebbf42f
Harrison Bouche <harrison@sm.media>
2023-11-22 09:11:09 -0500

    fix: partner logos urls

--- src/components/Partner.svelte
+++ src/components/Partner.svelte
@@ -22,12 +22,10 @@
 		>
 	</h3>
 	<div class="relative h-16 w-16 min-w-fit">
-		<div class="bg-primary absolute h-[4.5rem] w-[4.5rem] -translate-x-2"></div>
+		<div class="absolute h-[4.5rem] w-[4.5rem] -translate-x-2 bg-primary"></div>
 		<div class="absolute h-[4.5rem] w-[4.5rem] -translate-y-2 bg-black"></div>
 		<img
-			src={`${import.meta.env.CMS_URL}${
-				partner.logo_image.sizes?.logo.url ?? ""
-			}`}
+			src={partner.logo_image.sizes?.logo.url ?? ""}
 			alt=""
 			class="relative block h-16 w-16 shrink-0 list-none bg-white p-2"
 		/>
