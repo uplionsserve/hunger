bdf68aafa956ca107645b67be0903aee87f69b24
Harrison Bouche <harrison@sm.media>
2023-11-22 09:01:08 -0500

    fix: logo props, positioning issues

--- src/components/Partner.svelte
+++ src/components/Partner.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	type Partner = {
 		name: string
-		logo_image: string
+		logo_image: {
+			sizes: {
+				logo: {
+					url: string
+				}
+			}
+			alt: string
+		}
 		link: string
 	}
 
@@ -9,12 +16,12 @@
 </script>
 
 <li class="flex items-center gap-4">
-	<h3 class="order-2 max-w-[300px] grow">
-		<a href={partner.link} class="no-underline focus:outline-2" tabindex="0"
+	<h3 class="order-2 w-fit">
+		<a href={partner.link} class="focus:outline-2" tabindex="0"
 			>{partner.name}</a
 		>
 	</h3>
-	<div class="relative">
+	<div class="relative h-16 w-16 min-w-fit">
 		<div class="bg-primary absolute h-[4.5rem] w-[4.5rem] -translate-x-2"></div>
 		<div class="absolute h-[4.5rem] w-[4.5rem] -translate-y-2 bg-black"></div>
 		<img
