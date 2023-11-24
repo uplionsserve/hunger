55f6cca7a905d9b5a293cfc87350dafa41970a77
Harrison Bouche <harrison@sm.media>
2023-11-24 08:04:10 -0500

    feat: LinkButton can now be a button

--- src/components/LinkButton.svelte
+++ src/components/LinkButton.svelte
@@ -1,18 +1,31 @@
 <script lang="ts">
-	export let href: string = "#"
+	import { createEventDispatcher } from "svelte"
+
+	export let href: string | undefined = undefined
 	export let classes: string = ""
+
+	const dispatch = createEventDispatcher()
+	function click() {
+		dispatch("click")
+	}
 </script>
 
-<a {href} class="shadow-before relative h-fit w-fit no-underline {classes}">
+<svelte:element
+	this={href ? "a" : "button"}
+	{href}
+	class="shadow-before relative h-fit w-fit no-underline {classes}"
+	on:click={click}
+	role={href ? undefined : "button"}
+>
 	<span
 		class="bg-primary block w-fit px-5 py-3 text-lg font-bold uppercase text-white no-underline hover:brightness-90"
 	>
 		<slot />
 	</span>
-</a>
+</svelte:element>
 
 <style>
-	a::before {
+	.shadow-before::before {
 		content: "";
 		display: block;
 		width: 100%;
@@ -25,7 +38,7 @@
 		background-color: black;
 		transition: all 0.1s ease-in-out;
 	}
-	a:hover::before {
+	.shadow-before:hover::before {
 		transform: translate(0, 0);
 		opacity: 0;
 	}
