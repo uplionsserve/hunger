b39a73f7a7e53d74ddda8a510aae4ae05e9f5fc5
Harrison Bouche <harrison@sm.media>
2023-11-26 17:18:43 -0500

    feat: program listings now can show gallery images

--- src/components/ProgramListing.svelte
+++ src/components/ProgramListing.svelte
@@ -1,4 +1,14 @@
 <script lang="ts">
+	import { onMount } from "svelte"
+	import Carousel from "./Carousel/Carousel.svelte"
+	import LinkButton from "./LinkButton.svelte"
+
+	type Image = {
+		src: string
+		alt: string
+		caption: string
+	}
+
 	type Program = {
 		title: string
 		title_id: string
@@ -15,39 +25,60 @@
 			text: string
 			url: string
 		}
+		gallery_images: Array<{
+			image: {
+				url: string
+				alt: string
+			}
+			caption: string
+		}>
 	}
 
 	export let program: Program
+
+	let images: Image[] =
+		program.gallery_images.map((image) => ({
+			src: image.image.url,
+			alt: image.image.alt,
+			caption: image.caption,
+		})) || []
 </script>
 
 <section
 	id={program.title_id}
-	class="root md:max-w-1xl flex min-h-[300px] flex-col gap-4 md:mx-auto lg:col-span-8"
+	class="root md:max-w-1xl grid min-h-[300px] gap-4 md:mx-auto lg:grid-cols-3"
 >
-	<header class="mb-4 mt-1 flex items-center gap-2">
-		{#if program.logo_image}
-			<img
-				src={`${program.logo_image.sizes?.logo.url ?? ""}`}
-				alt=""
-				class="absolute h-24 w-24 opacity-20 lg:static lg:h-20 lg:w-20 lg:opacity-100"
-			/>
+	<div class="col-span-2">
+		<header class="mb-4 mt-1 flex items-center gap-4">
+			{#if program.logo_image}
+				<img
+					src={`${
+						import.meta.env.PROD
+							? program.logo_image.sizes?.logo.url
+							: `http://localhost:3000${program.logo_image.sizes?.logo.url}` ??
+							  ""
+					}`}
+					alt=""
+					class="absolute h-24 w-24 opacity-20 lg:static lg:h-20 lg:w-20 lg:opacity-100"
+				/>
+			{/if}
+			<h2
+				class="relative mt-8 text-4xl font-extrabold uppercase leading-10 tracking-tight text-gray-900 sm:text-6xl sm:leading-none lg:mt-0 lg:text-5xl xl:text-6xl"
+			>
+				{program.title}
+			</h2>
+		</header>
+		{@html program.description_html}
+		{#if program.cta_button.url}
+			<LinkButton href={program.cta_button.url} classes="mt-4">
+				{program.cta_button.text}
+			</LinkButton>
 		{/if}
-		<h2
-			class="relative mt-8 text-4xl font-extrabold uppercase leading-10 tracking-tight text-gray-900 sm:text-6xl sm:leading-none lg:mt-0 lg:text-5xl xl:text-6xl"
-		>
-			{program.title}
-		</h2>
-	</header>
-
-	{@html program.description_html}
-
-	{#if program.cta_button.url}
-		<a
-			href={program.cta_button.url}
-			target="_blank"
-			class="bg-primary w-fit px-5 py-3 font-bold uppercase text-white no-underline hover:brightness-90"
-			>{program.cta_button.text}</a
-		>
+	</div>
+	{#if images.length > 0}
+		<div>
+			<Carousel {images} />
+		</div>
 	{/if}
 </section>
 
