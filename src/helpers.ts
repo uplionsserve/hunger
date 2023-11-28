d4f340304925e8c4dd5236fea5f0e249d94933db
Harrison Bouche <harrison@sm.media>
2023-11-28 10:03:00 -0500

    fea: API key auth

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,6 +2,11 @@ export async function getCMSData(dataType: string) {
 	try {
 		const dataRes = await fetch(
 			`${import.meta.env.CMS_URL}/api/hunger-data-${dataType}`,
+			{
+				headers: {
+					Authorization: `api-keys API-Key ${import.meta.env.CMS_API_KEY}`,
+				},
+			},
 		)
 		if (dataRes.ok) {
 			return await dataRes.json()
@@ -17,6 +22,11 @@ export async function getCMSPage(pageType: string) {
 	try {
 		const pageRes = await fetch(
 			`${import.meta.env.CMS_URL}/api/globals/hunger-pages-${pageType}`,
+			{
+				headers: {
+					Authorization: `api-keys API-Key ${import.meta.env.CMS_API_KEY}`,
+				},
+			},
 		)
 		if (pageRes.ok) {
 			return await pageRes.json()
